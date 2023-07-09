43d17c1e02ba6d6823ed7bb368b3a32d855ad66d
jromero417 <jacobromero417@gmail.com>
2023-07-09 09:41:30 -0500

    The feature Planet1 is added and working fine

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,11 @@ public class Main {
         Omnax.Sun="1";
         Omnax.Planets="2";
         Omnax.Star="4";
+
+        Planet1 xeon = new Planet1();
+        xeon.name="reon";
+        xeon.color="pink";
+        xeon.temp="-45 celcius";
+        xeon.size="1 cm";
     }
 }
